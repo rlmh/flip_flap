0e05d89c6662ae85b3086b670fda4e7add642c95
Luis Herrera <lmherrera86@gmail.com>
2015-10-02 00:33:17 +0800

    working tsv parsing

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,23 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    # split the records by new line
+    temp = tsv.split(/[\n]/)
+    # get the field names (keys) which are in the first record
+    keys = temp[0].split(/[\t]/)
+    temp.shift
+    #do a mapping by transposing
+    @data = Array.new
+    temp.map do |rec|
+      @data << Hash[[ keys,rec.split("\t") ].transpose]
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    @data[0].map { |k, v| k }.join("\t").concat( "\n" +
+      @data.map {|ak, bv| ak.map {|k, v| "#{v}"}.join("\t")}.join("\n")
+    ) + "\n"
   end
 end
